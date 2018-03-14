4f1e80787e05c2dbf01b985e10b3744111e22053
Rachel Ungar <rachelungar@gmail.com>
2018-03-14 18:04:23 -0500

    fix output formatting errors and major awk bugs
    still major errors in matrix creation loops

--- make.matrix.sh
+++ make.matrix.sh
@@ -14,23 +14,27 @@
 
 for ArchaeType in Archae Archaeless
 do
-
 num=5
 for level in order family genus
 do
+	echo "$ArchaeType - $level"
 	num=`expr $num + 1`
-	awk -v num=$num '{print $num}' ArchaeType/* | sed '1d' | sort -u > bac.cols.$level.txt
-	printf "bacteria\t"
-	awk -vRS="\n" -vORS="\t" '1' bac.cols.$level.txt
-	printf "\n"
+	awk -v num="$num" '{print $num}' $ArchaeType/* | sed '1d' | sort -u > bac.cols.$level.txt
+	printf "bacteria\t" > matrix.$ArchaeType.$level.txt
+	awk -vRS="\n" -vORS="\t" '1' bac.cols.$level.txt >> matrix.$ArchaeType.$level.txt
+	printf "\n" >> matrix.$ArchaeType.$level.txt
 
-	for file in $(ls ArchaeType/)
+	for file in $(ls $ArchaeType/)
 	do
-		header=`echo $file | rev | cut -c18- | rev | cut -d'/' -f5 | cut -d '.' -f2-`
+		header=`echo $file | rev | cut -c18- | rev | cut -d'/' -f5 | cut -d '.' -f3-`
+		
+		#source for next line: https://www.unix.com/shell-programming-and-scripting/73557-awk-hash-function.html
+		#this next line makes the hash and saves it in a file
+		awk -v num=$num '{cnt[$num]+=1}END{for (x in cnt){print x,cnt[x]}}' $ArchaeType/$file | sort > hash.temp.txt
 		printf "$header\t"
 		while read cols
 		do
-  			num=0
+  			count=0
 	
 			while read line
         		do
@@ -39,15 +43,16 @@ do
 	
         	        	if [ "$line1" == "$cols" ]
                 		then
-                		    	num=$line2
+                		    	count=$line2
                 		fi
-        		done <test
+        		done <hash.temp.txt
 
-        		printf  "$num\t"
+        		printf  "$count\t"
 
-		done <bac.cols.$line.txt
+		done <bac.cols.$level.txt
 		echo ""
-	done > matrix.$line.txt
+		rm hash.temp.txt
+	done >> matrix.$ArchaeType.$level.txt
 done
 
 done
