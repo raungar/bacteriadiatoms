046985dd55a8916f4644addc2d4125b0bf685718
Rachel Ungar <rachelungar@gmail.com>
2018-04-21 20:11:01 -0500

    first commit
    this takes the header file from longest.in.cluster.python and edits the fasta to match it

---
+++ editheaders.cdhits.py
@@ -0,0 +1,22 @@
+from Bio import SeqIO
+import re
+
+
+clstrfile = open("cdhits.header.txt", "r")
+
+fasta_sequences = SeqIO.parse(open("cdhits"),'fasta')
+#with open("editheaders.cdhits.fasta") as out_file:
+for fasta in fasta_sequences:
+	name, sequence = fasta.id, str(fasta.seq)
+	full_header = re.split("<", name)
+	header=full_header[0] + "<" + full_header[1]
+	#header="TRINITY"
+	
+	clstrfile.seek(0)
+
+	for line in clstrfile:
+		if header in line:
+			name=line
+			found="true"
+			break	
+	print(name,sequence,sep='')
