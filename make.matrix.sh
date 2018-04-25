340198bb24a86a3e7dfebe924df89c39a27fe4ca
Rachel Ungar <rachelungar@gmail.com>
2018-04-25 09:46:04 -0500

    adjust for filtering process

--- make.matrix.sh
+++ make.matrix.sh
@@ -2,17 +2,17 @@
 
 
 #make sure and run archae.sh before
-for ArchaeType in Archae Archaeless
+for ArchaeType in ArchaeaSSU ArchaealessSSU
 do
 num=5 #this is which column will be awk-ed for order/family/genus/species
-for level in order family genus
+for level in order family genus species
 do
 	echo "$ArchaeType - $level"
 	num=`expr $num + 1` #change taxonomic level
-	awk -v num="$num" '{print $num}' $ArchaeType/* | sed '1d' | sort -u > MatrixOutput/bac.cols.$ArchaeType.$level.txt #get all unique tax ids in level
-	printf "bacteria\t" > MatrixOutput/matrix.$ArchaeType.$level.txt
-	awk -vRS="\n" -vORS="\t" '1' MatrixOutput/bac.cols.$ArchaeType.$level.txt >> MatrixOutput/matrix.$ArchaeType.$level.txt #print uniq tax ids tab delimited
-	printf "\n" >> MatrixOutput/matrix.$ArchaeType.$level.txt 
+	awk -v num="$num" '{print $num}' $ArchaeType/* | sed '1d' | sort -u > MatrixOutputSSU/bac.cols.$ArchaeType.$level.txt #get all unique tax ids in level
+	printf "bacteria\t" > MatrixOutputSSU/matrix.$ArchaeType.$level.txt
+	awk -vRS="\n" -vORS="\t" '1' MatrixOutputSSU/bac.cols.$ArchaeType.$level.txt >> MatrixOutputSSU/matrix.$ArchaeType.$level.txt #print uniq tax ids tab delimited
+	printf "\n" >> MatrixOutputSSU/matrix.$ArchaeType.$level.txt 
 	#this section creates the header of the matrix
 	
 
@@ -22,7 +22,7 @@ do
 		
 		#source for next line: https://www.unix.com/shell-programming-and-scripting/73557-awk-hash-function.html
 		#this next line makes a hash and then outputs 2 columns: one with the tax id and one with the count
-		awk -v num=$num '{cnt[$num]+=1}END{for (x in cnt){print x,cnt[x]}}' $ArchaeType/$file | sort > hash.temp.txt
+		awk -v num1=$num1 -v num2=$num2 -v num3=$num3 '{cnt[$num1 " " $num2 " " $num3]+=1}END{for (x in cnt){print x,cnt[x]}}' $ArchaeType/$file | sort > hash.temp.txt
 		printf "$header\t"
 
 		#read col name (tax id), then looks to see if that tax id is in hash file
@@ -34,21 +34,28 @@ do
 			#this reads through hash file
 			while read line
         		do
-          			line1=$(echo $line | cut -d ' ' -f 1) #name of tax id
-                		line2=$(echo $line | cut -d ' ' -f 2) #count of tax id
-	
-        	        	if [ "$line1" == "$cols" ]
-                		then
-                		    	count=$line2 
-                		fi
+          			line1=$(echo $line | cut -d ' ' -f 1,2,3) #name of tax id
+                		line2=$(echo $line | cut -d ' ' -f 4) #count of tax id
+				isna=$($(echo $line | cut -d ' ' -f 3) #species is NA
+				
+				if [ $isna == "NA" ]
+				then
+					
+				else
+        	        		if [ "$line1" == "$cols" ]
+                			then
+                		    		count=$line2 
+                			fi
+				fi
+
         		done <hash.temp.txt
 
         		printf  "$count\t" #nothing found: prints zero, found: print number from file 
 
-		done <MatrixOutput/bac.cols.$ArchaeType.$level.txt
+		done <MatrixOutputSSU/bac.cols.$ArchaeType.$level.txt
 		echo ""
 		rm hash.temp.txt
-	done >> MatrixOutput/matrix.$ArchaeType.$level.txt
+	done >> MatrixOutputSSU/matrix.$ArchaeType.$level.txt
 
 done
 
