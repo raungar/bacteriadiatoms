d7b3837a9a5ab0033e0a8fd1683f16db146ad500
Rachel Ungar <rachelungar@gmail.com>
2018-04-26 14:10:31 -0500

    redo to fit SSU formatting

--- make.matrix.species.sh
+++ make.matrix.species.sh
@@ -1,38 +1,52 @@
 #!/bin/bash
 
-for ArchaeType in Archae Archaeless
+for ArchaeType in ArchaeaSSU ArchaealessSSU
 do
+
+if [ $ArchaeType == "ArchaeaSSU" ]
+then
+	arc="archaea"
+else
+	arc="archaealess"
+fi
+
+num=5
+
 for level in species
 do
 	echo "$ArchaeType - $level"
-	#num=`expr $num + 1`
-	num1=7
-	num2=8
-	num3=9
+	num=`expr $num + 1`
+	num1=$num
+	num2=`expr $num1 + 1`
+	num3=`expr $num2 + 1`
+	num4=`expr $num3 + 1`
 
-	awk -v num1="$num1" -v num2="$num2" -v num3="$num3" '{if ($num3 == "NA"){print $num3} else{print $num1"."$num2"."$num3}}' $ArchaeType/* | sed '1d' | sort -u > MatrixOutput/bac.cols.$ArchaeType.$level.txt
-	printf "bacteria\t" > MatrixOutput/matrix.$ArchaeType.$level.txt
-	awk -vRS="\n" -vORS="\t" '1' MatrixOutput/bac.cols.$ArchaeType.$level.txt >> MatrixOutput/matrix.$ArchaeType.$level.txt
-	printf "\n" >> MatrixOutput/matrix.$ArchaeType.$level.txt
+	awk -v num1="$num1" -v num2="$num2" -v num3="$num3" -v num4="$num4" '{print $num1"_"$num2"_"$num3"_"$num4}' $ArchaeType/Diatoms/* | sed '1d' | sort -u > MatrixOutputSSU/bac.cols.$ArchaeType.$level.txt
+		printf "bacteria\t" > MatrixOutputSSU/matrix.$ArchaeType.$level.txt
+	awk -vRS="\n" -vORS="\t" '1' MatrixOutputSSU/bac.cols.$ArchaeType.$level.txt >> MatrixOutputSSU/matrix.$ArchaeType.$level.txt
+	printf "\n" >> MatrixOutputSSU/matrix.$ArchaeType.$level.txt
 
 
-	for file in $(ls $ArchaeType/)
+	for file in $(ls "$ArchaeType/Diatoms/")
 	do
-		header=`echo $file | rev | cut -c18- | rev | cut -d'/' -f5 | cut -d '.' -f3-`
-		
 		#source for next line: https://www.unix.com/shell-programming-and-scripting/73557-awk-hash-function.html
 		#this next line makes the hash and saves it in a file
-		awk -v num1=$num1 -v num2=$num2 -v num3=$num3 '{if ($num3 == "NA"){ cnt[$num3]+=1} else{ cnt[$num1"."$num2"."$num3]+=1}}END{for (x in cnt){print x,cnt[x]}}' $ArchaeType/$file | sort > hash.temp.txt
+		awk -v num1=$num1 -v num2=$num2 -v num3=$num3 -v num4="$num4"'{if ($num4 == "NA"){ {cnt[$num1"_"$num2"_"$num3"_"$num4]+=1}END{for (x in cnt){print x,cnt[x]}}' "$ArchaeType/Diatoms/$file" | sort > hash.temp.txt
+		
+		header=`echo $file | awk -F"_" '{print $1}'`
 		printf "$header\t"
+
 		while read cols
 		do
   			count=0
-	
 			while read line
         		do
+
+			
 			  	line1=$(echo $line | cut -d ' ' -f 1) #name of tax id
                                 line2=$(echo $line | cut -d ' ' -f 2) #count of tax id
                                 isna=$(echo $line | cut -d ' ' -f1 | cut -d '.' -f3) #species is NA
+				#printf "$line1 $line2"
 
                                 if [ "$isna" == "NA" ]
                                 then
@@ -48,12 +62,11 @@ do
 
         		printf  "$count\t"
 
-		done <MatrixOutput/bac.cols.$ArchaeType.$level.txt
+		done <MatrixOutputSSU/bac.cols.$ArchaeType.$level.txt
 		echo ""
 		rm hash.temp.txt
-	done >> MatrixOutput/matrix.$ArchaeType.$level.txt
 
-
+	done >> MatrixOutputSSU/matrix.$ArchaeType.$level.txt
 done
 
 done
