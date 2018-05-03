0a24f50eacc140ce56b86124daae64ad0266411e
Rachel Ungar <rachelungar@gmail.com>
2018-05-03 10:35:46 -0500

    > to -
    full_header = re.split(">", name) --> full_header = re.split("-", name)

--- editheaders.cdhits.py
+++ editheaders.cdhits.py
@@ -8,7 +8,7 @@ fasta_sequences = SeqIO.parse(open("cdhits"),'fasta')
 #with open("editheaders.cdhits.fasta") as out_file:
 for fasta in fasta_sequences:
 	name, sequence = fasta.id, str(fasta.seq)
-	full_header = re.split("<", name)
+	full_header = re.split("-", name)
 	header=full_header[0] + "<" + full_header[1]
 	#header="TRINITY"
 	
