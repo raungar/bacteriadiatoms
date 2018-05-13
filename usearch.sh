972df592ed83bd59f8630dbbae5d3fb14683908a
Rachel Ungar <rachelungar@gmail.com>
2018-05-12 21:50:47 -0500

    update for directory changes

--- usearch.sh
+++ usearch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-for files in $(ls diatomdir_isoformless/)
+#for loop: make sure you use directory after remove isoforms
+for files in $(ls DiatomIsoform/)
 do
-	usearch -cluster_fast $files -id 1 -centroids "diatomdir_usearch/$files" 
+
+        /share/apps/bioinformatics/usearch/10.0.240/usearch -cluster_fast /home/raungar/DiatomIsoform/$files -id 1 -centroids "DiatomUsearch/temp.$files"
+
+        #taken from https://www.biostars.org/p/9262/
+        
+	awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' "DiatomUsearch/temp.$files" | sed '1d' > "DiatomUsearch/$files"
+        rm "DiatomUsearch/temp.$files"
 done
