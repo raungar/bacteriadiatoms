a953e671a9db547c7379caee26224de20803fe0c
Rachel Ungar <rachelungar@gmail.com>
2018-05-14 12:16:15 -0500

    formatting

--- editheaders.cdhits.py
+++ editheaders.cdhits.py
@@ -7,16 +7,16 @@ clstrfile = open("cdhits.header.txt", "r")
 fasta_sequences = SeqIO.parse(open("cdhits"),'fasta')
 #with open("editheaders.cdhits.fasta") as out_file:
 for fasta in fasta_sequences:
-	name, sequence = fasta.id, str(fasta.seq)
-	full_header = re.split("-", name)
-	header=full_header[0] + "<" + full_header[1]
-	#header="TRINITY"
-	
-	clstrfile.seek(0)
+        name, sequence = fasta.id, str(fasta.seq)
+        full_header = re.split("<", name)
+        header=full_header[0] + "<" + full_header[1]
 
-	for line in clstrfile:
-		if header in line:
-			name=line
-			found="true"
-			break	
-	print(">",name,sequence,sep='')
+        clstrfile.seek(0)
+
+        for line in clstrfile:
+                if header in line:
+                        name=line.strip()
+                        found="true"
+                        break
+        print(name)
+        print(sequence,"\n",sep='')
