81a6686fc2d6f272a6a7125d3e29bdad563e5c2d
Rachel Ungar <rachelungar@gmail.com>
2018-05-30 09:05:02 -0500

    update for new workflow

--- archae.sh
+++ archae.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
+#takes reduced phylip files and reduce dada2 names to the reduced list
+# 
 
-for files in $(ls DadaOutputGG3/)
+
+cat b.raxml/ssu-cdhits.masked.bateria.phy | grep TRINITY | awk -F"<" '{print $1" "$2}' > bacteria.headers      
+cat a.raxml/ssu-cdhits.masked.archaea.phy | grep TRINITY | awk -F"<" '{print $1" "$2}' > archaea.headers 
+
+cat DiatomNoCM/*  > diatomnocm.txt
+cat diatomnocm.txt | wc -l
+
+rm Archae/archaea.txt
+touch Archae/archaea.txt
+rm Archaeless/archaealess.txt
+touch Archaeless/archaealess.txt
+
+rm removed.ssu.cdhits.txt
+touch removed.ssu.cdhits.txt
+
+while read line
 do
+	header=`echo $line | awk '{print $1" "$2}'`
+	#echo $header
+	if grep -Fxq "$header" bacteria.headers
+        then
+		echo "$line" >> Archaeless/archaealess.txt
+	elif grep -Fxq "$header" archaea.headers
+	then
+		echo "$line" >> Archae/archaea.txt
+	else
+		echo $header >> removed.ssu.cdhits.txt
+	fi
 
-        cat "DadaOutputGG3/$files" | sed "s/-/./g" | grep "Archae" > "Archae/archae.$files"
-        cat "DadaOutputGG3/$files" | sed "s/-/./g" | grep -v "Archae" > "Archaeless/archaeless.$files" #easy just grep -v to filter out other things $
-
-done
+done<diatomnocm.txt
